bc2b4aea89580878dcd7da26f0cabd4b69c0e091
Pawan Kumar <pawan@tykeinvest.com>
2024-04-08 16:26:22 +0530

    code cleanup and deprecation

--- nested.go
+++ nested.go
@@ -1,62 +1,62 @@
 package nested
 
 import (
-	"errors"
 	"fmt"
 	"strconv"
 	"strings"
 )
 
 // Get takes data map[string]any and keys ...string and it searches for value
-// nested within data using keys. for example data["key1"]["key2"]["key3"].
+// nested within data using keys. i.e. data["key1"]["key2"]["key3"].
 //
-// if any previous key returns array then it tries to convert current key into integer for indexing.
+// if a previous key returns array then it tries to convert current key into integer for indexing.
 //
-// if any previous key returns non-indexable (number, string, boolean) and there are remaining keys to search
+// if a previous key returns non-indexable (number, string, boolean) and there are remaining keys to search
 // then it will return error and returns closest value it can find
-func Get(data map[string]any, keys ...string) (any, error) {
-	value := any(data)
-	for _, key := range keys {
-		if isObject(value) {
-			if temp, ok := value.(map[string]any)[key]; !ok {
-				return temp, errors.New(fmt.Sprintf("key doesn't exist or nil %v", key))
-			} else {
-				value = temp
+func Get(data map[string]interface{}, keys ...string) (any, error) {
+	v := any(data)
+	for _, k := range keys {
+		switch val := v.(type) {
+		case map[string]any:
+			temp, ok := val[k]
+			if !ok {
+				return temp, fmt.Errorf("either key '%s' doesn't exist or is nil", k)
 			}
-		} else if isArray(value) {
-			index, err := strconv.ParseInt(key, 10, 64)
-			if err != nil {
-				return value, errors.New("got array but key can not be converted to int")
+			v = temp
+		case []interface{}:
+			index, err := strconv.Atoi(k)
+			if err != nil || index < 0 || index >= len(val) {
+				return v, fmt.Errorf("can not index into array using %v", k)
 			}
-			if index >= int64(len(value.([]any))) {
-				return value, errors.New("index out of bound")
-			}
-			value = value.([]any)[index]
-		} else {
-			// there is a key that it need to get but the prev value is neither map nor array
-			return value, errors.New("previous value is not (array or map)")
+			v = val[index]
+		default:
+			return v, fmt.Errorf("previous value is neither map[string]any nor []any")
 		}
 	}
-	return value, nil
+	return v, nil
 }
 
-// Gets() calls Get() after spliting key on dot (.)
-func Gets(data map[string]any, key string) (any, error) {
-	return Get(data, strings.Split(key, ".")...)
-}
-
-// Panic version of Get()
-func GetP(data map[string]any, keys ...string) any {
+func MustGet(data map[string]any, keys ...string) any {
 	value, err := Get(data, keys...)
 	if err != nil {
-		panic(fmt.Sprintf("failed to get %v", strings.Join(keys, ".")))
+		panic(err)
 	}
 	return value
 }
 
-// Panic version of Gets()
+// Deprecated: use Get(data, strings.Split("key1.key2.key3", ".")
+func Gets(data map[string]any, key string) (any, error) {
+	return Get(data, strings.Split(key, ".")...)
+}
+
+// Deprecated: use MustGet(data, "key1", "key2")
+func GetP(data map[string]any, keys ...string) any {
+	return MustGet(data, keys...)
+}
+
+// Deprecated: use MustGet(data, strings.Split("key1.key2.key3", ".")
 func GetsP(data map[string]any, key string) any {
-	value, err := Gets(data, key)
+	value, err := Get(data, strings.Split(key, ".")...)
 	if err != nil {
 		panic(fmt.Sprintf("failed to get %v", key))
 	}
